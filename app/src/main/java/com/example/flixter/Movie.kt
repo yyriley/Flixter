fe91304684f8881b8a6b56082219a1b30c95f32d
Yrua Riley <yyriley0124@gmail.com>
2022-03-04 01:42:54 -0600

    Implemented details screen

--- app/src/main/java/com/example/flixter/Movie.kt
+++ app/src/main/java/com/example/flixter/Movie.kt
@@ -1,15 +1,22 @@
 package com.example.flixter
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
+@Parcelize
 // Represent one movie object to display in UI
 data class Movie(
     val movieID: Int,
+    val voteAverage: Double,
     private val posterPath: String,
     val title: String,
     val overview: String,
-) {
+) : Parcelable {
+    @IgnoredOnParcel
     val posterImageURL = "https://image.tmdb.org/t/p/w342/$posterPath"
+
     // A companion object allows us to call a method in a class without having
     // to declare an instance
     companion object {
@@ -20,6 +27,7 @@ data class Movie(
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("title"),
                         movieJson.getString("overview")
